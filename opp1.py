b50af0807003b2a7dba2c5f2b0a42ba6e9a76bef
Harsh prasad <harshprasad1204@gmail.com>
2026-07-28 22:47:46 +0530

    class properties

--- opp1.py
+++ opp1.py
@@ -2,15 +2,20 @@
 # Almost everything in python is a object, with properties and method.
 
 # class Person:
-#     name = " Harsh"
+#     name = " Harsh" # class property
 #     occupation = "Software Engineer"
 #     becam = "Billionaire"
-#     def info(self):
+#     def info(self):   # method
 #         print(f'{self.name} is a {self.occupation}')
 
 # a = Person() # creating object using class Person
 # print(a.name)
 # a.info()
+'''
+All methods must have self as the first parameter.
+
+
+'''
 
 # __ini__ method in opps
 '''
@@ -24,10 +29,16 @@ The __init__() method is called automatically every time
 
 class Person:
     def __init__(self,name,age):
-        self.name = name
+        self.name = name # instance property
         self.age = age
 
 p1 = Person("Harsh",21)
 print(p1.name)
 print(p1.age)
 print(f'name is {p1.name} and age {p1.age}')
+
+'''
+self parameter is a refeerence to the current instance of the class.
+it is used to access properties and methods that belong to the class.
+While you can use a different name, it is strongly recommended to use self as it is the convention in Python and makes your code more readable to others.
+'''
