798499e99f9cd210d0a1eecc93d16df1a1d0c2a8
Harsh prasad <harshprasad1204@gmail.com>
2026-07-28 11:40:39 +0530

    lambda funtion

---
+++ lambda.py
@@ -0,0 +1,14 @@
+# lambda function is a small anonymous function without a name. it is defined using the lambda keyword.
+
+# def double(x):
+#     print(x*2)
+
+# double(5)
+
+double = lambda x:x*2
+
+print(double(5))
+
+avg = lambda x,y: (x+y)/2
+print(avg(8,4))
+
